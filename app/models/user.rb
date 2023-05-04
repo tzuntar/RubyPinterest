44656cd253645cab4c141140af4f058218d69201
Tobija Žuntar <tobija.zuntar@gmail.com>
2023-05-04 09:04:05 +0200

    Enable user confirmations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        # omogoci v produkciji :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :boards, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :pins, through: :boards, dependent: :destroy
